ea50b17dc1830e3e5bbd48f83d273e97335e98d0
Luis Leal <luisrrleal@gmail.com>
2024-01-11 14:40:43 -0600

    Register working and saving it to db

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,8 @@
+import 'package:app_cafe/config/router/app_router_notifier.dart';
+import 'package:app_cafe/features/auth/presentation/providers/auth_provider.dart';
 import 'package:app_cafe/features/products/domain/entities/product.dart';
 import 'package:app_cafe/features/products/presentation/screens/product_info_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/auth/presentation/screens/screens.dart';
@@ -19,38 +22,70 @@ final product = Product(
 );
 
 // final appRouter = GoRouter
-final appRouter = GoRouter(initialLocation: '/home/0', routes: [
-  GoRoute(
-      path: '/home/:page',
-      name: HomeScreen.name,
-      builder: (context, state) {
-        final pageIndexString = state.pathParameters['page'] ?? '0';
-        final pageIndex = int.parse(pageIndexString);
-        return HomeScreen(pageIndex: pageIndex);
-      },
-      routes: [
-        GoRoute(
-          path: 'food-info/:id',
-          builder: (context, state) => ProductInfoScreen(product: product),
-        )
-      ]),
-  GoRoute(path: '/', redirect: (_, __) => '/home/0'),
-  GoRoute(
-    path: '/login',
-    builder: (context, state) => const LoginScreen(),
-  ),
-  GoRoute(
-    path: '/register',
-    builder: (context, state) => const RegisterScreen(),
-  ),
-  GoRoute(
-    path: '/splash',
-    builder: (context, state) => const CheckAuthStatusScreen(),
-  ),
-  GoRoute(
-    path: '/food-info',
-    builder: (context, state) => ProductInfoScreen(
-      product: product,
-    ),
-  ),
-]);
+final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
+
+  return GoRouter(
+    initialLocation: '/splash',
+    refreshListenable: goRouterNotifier,
+    routes: [
+      GoRoute(
+          path: '/home/:page',
+          name: HomeScreen.name,
+          builder: (context, state) {
+            final pageIndexString = state.pathParameters['page'] ?? '0';
+            final pageIndex = int.parse(pageIndexString);
+            return HomeScreen(pageIndex: pageIndex);
+          },
+          routes: [
+            GoRoute(
+              path: 'food-info/:id',
+              builder: (context, state) => ProductInfoScreen(product: product),
+            )
+          ]),
+      GoRoute(path: '/', redirect: (_, __) => '/home/0'),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
+      GoRoute(
+        path: '/food-info',
+        builder: (context, state) => ProductInfoScreen(
+          product: product,
+        ),
+      ),
+    ],
+    redirect: (context, state) {
+      final isGoingTo = state.matchedLocation;
+      final authStatus = goRouterNotifier.authStatus;
+
+      if (isGoingTo == '/splash' && authStatus == AuthStatus.checking) {
+        return null;
+      }
+
+      if (authStatus == AuthStatus.notAuthenticated) {
+        if (isGoingTo == '/login' || isGoingTo == '/register') return null;
+
+        return '/login';
+      }
+
+      if (authStatus == AuthStatus.authenticated) {
+        if (isGoingTo == '/login' ||
+            isGoingTo == '/register' ||
+            isGoingTo == '/splash') {
+          return '/';
+        }
+      }
+
+      return null;
+    },
+  );
+});
--- lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
+++ lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -1,49 +1,50 @@
-import 'package:app_cafe/features/shared/shared.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'key_value_storage_service.dart';
 
-class KeyValueStorageServiceImpl implements KeyValueStorageService {
-  Future<SharedPreferences> getSharedPreferences() async {
+class KeyValueStorageServiceImpl extends KeyValueStorageService {
+  Future<SharedPreferences> getSharedPrefs() async {
     return await SharedPreferences.getInstance();
   }
 
   @override
   Future<T?> getValue<T>(String key) async {
-    final preferences = await getSharedPreferences();
+    final prefs = await getSharedPrefs();
 
     switch (T) {
-      case (int):
-        preferences.getInt(key) as T;
-        break;
+      case int:
+        return prefs.getInt(key) as T?;
 
-      case (String):
-        preferences.getString(key) as T;
-        break;
+      case String:
+        return prefs.getString(key) as T?;
 
       default:
         throw UnimplementedError(
-            'GET KeyValue not implemented for type ${T.runtimeType}');
+            'GET not implemented for type ${T.runtimeType}');
     }
   }
 
   @override
   Future<bool> removeKey(String key) async {
-    final preferences = await getSharedPreferences();
-    return await preferences.remove(key);
+    final prefs = await getSharedPrefs();
+    return await prefs.remove(key);
   }
 
   @override
   Future<void> setKeyValue<T>(String key, T value) async {
-    final preferences = await getSharedPreferences();
+    final prefs = await getSharedPrefs();
+
     switch (T) {
       case int:
-        preferences.setInt(key, value as int);
+        prefs.setInt(key, value as int);
         break;
+
       case String:
-        preferences.setString(key, value as String);
+        prefs.setString(key, value as String);
         break;
+
       default:
         throw UnimplementedError(
-            'Set KeyValue not implemented for type ${T.runtimeType}');
+            'Set not implemented for type ${T.runtimeType}');
     }
   }
 }
