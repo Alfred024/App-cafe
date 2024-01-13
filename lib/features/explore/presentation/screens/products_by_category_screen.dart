f89a395aa4a851e344152a4ca2508a33784966e6
Alfredo <21030761@itcelaya.edu.mx>
2024-01-13 12:05:53 -0600

    Creation of _ProductSlide at ProductsCategoryScreen

--- lib/features/explore/presentation/screens/products_by_category_screen.dart
+++ lib/features/explore/presentation/screens/products_by_category_screen.dart
@@ -1,3 +1,5 @@
+import 'package:app_cafe/config/config.dart';
+import 'package:app_cafe/features/products/domain/entities/product.dart';
 import 'package:flutter/material.dart';
 // Fernando Herrera animate_do
 import 'package:animate_do/animate_do.dart';
@@ -22,9 +24,79 @@ class ProductsByCategoryScreen extends ConsumerWidget {
           ),
           title: Text('Category: $category'),
         ),
-        body: const Center(
-          child: Text('Hacer el UI de los productos filtrados'),
-        ),
+        body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            itemCount: 30,
+            itemBuilder: (context, index) {
+              //final product = products[index];
+              return const _ProductSlide();
+            }),
+      ),
+    );
+  }
+}
+
+class _ProductSlide extends StatelessWidget {
+  // final Product product;
+  const _ProductSlide();
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = AppTheme().getTheme().textTheme;
+
+    return Container(
+      height: 400,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  height: 240,
+                  fit: BoxFit.cover,
+                  'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress != null) {
+                      return const Center(
+                          child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                      ));
+                    }
+                    return GestureDetector(
+                        // onTap: () => context.push('/home/0/food-info/productId'),
+                        onTap: () {},
+                        child: FadeIn(
+                          child: child,
+                        ));
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text(
+                'Espagueti',
+                style: textTheme.titleLarge,
+              ),
+              const Spacer(),
+              Text(
+                '\$70',
+                style: textTheme.displayLarge,
+              )
+            ],
+          ),
+          const Text(
+              'Espagueto cons espinacasy unas salchicias. voy a poner una descripción más greande para ver cómo queda en el UI final.')
+        ],
       ),
     );
   }
